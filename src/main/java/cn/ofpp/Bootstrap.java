188596af85ddd94544682fc4b5df7d0d311baae5
Hiraeth <2670428853@qq.com>
2022-08-25 10:22:56 +0800

    Initial commit

---
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -0,0 +1,39 @@
+package cn.ofpp;
+
+import cn.hutool.core.util.StrUtil;
+import cn.ofpp.core.Wx;
+
+/**
+ * 引导配置
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class Bootstrap {
+
+    /**
+     * 公众号AppID
+     */
+    public static final String APP_ID = "wx2d818c8c86d9a67d";
+
+    /**
+     * 公众号秘钥
+     */
+    public static final String SECRET = "35162a9e503b4a2b9944140295ed4c8a";
+
+    /**
+     * 全局模板ID  也可针对单个Friend指定模板
+     */
+    public static final String TEMPLATE_ID = "rNc-X_YGrIEInGL8BtmKN4yaMCptNXAXdsSImDxywIQ";
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        if (StrUtil.hasEmpty(APP_ID, SECRET, TEMPLATE_ID)) {
+            throw new IllegalArgumentException("请检查配置");
+        }
+        Wx.init();
+    }
+
+}
